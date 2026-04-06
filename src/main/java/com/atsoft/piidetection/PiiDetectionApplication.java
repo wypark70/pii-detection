1148b351ece208b46c4590cd335e1753ebc6b4b4
W.Y.Park <wypark70@gmail.com>
2026-04-07 04:30:04 +0900

    first commit

---
+++ src/main/java/com/atsoft/piidetection/PiiDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.atsoft.piidetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiiDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiiDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsoft/piidetection/PiiDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsoft.piidetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiiDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
